76e49677565c53cbfc0dfd7efd41646d2a283a7c
yoshiki_murakami <eiim.yoshiki@gmail.com>
2022-11-25 20:04:42 +0900

    change: ヘルパーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+
+    return page_title + " | " + base_title if page_title.present?
+    base_title
+  end
 end
