8be6cb566ef9a6dfb73517041a6ecdadaa169594
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-01-30 20:44:34 +1100

    add: フレンドリーフォワーディング

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -53,4 +53,9 @@ module SessionsHelper
   def current_user?(user)
     user && user == current_user
   end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
