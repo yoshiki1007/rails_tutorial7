db1f713cdf5758419fb6bb3389284032a4532b18
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-01-31 15:06:44 +1100

    Change build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
